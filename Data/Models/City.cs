fd3493ec56eaba81928f7fcf83212c39999835f0
Charles McQuain <charlesmcquain@gmail.com>
2020-06-19 23:02:02 -0700

    Created and defined City and Country models

---
+++ Data/Models/City.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorldCities.Data.Models
+{
+    public class City
+    {
+        #region Constructor
+        public City()
+        {
+
+        }
+        #endregion
+
+        #region Properties
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Name_ASCII { get; set; }
+        public decimal Lat { get; set; }
+        public decimal Lon { get; set; }
+        #endregion
+        [ForeignKey("Country")]
+        public int CountryId { get; set; }
+
+        #region Navigation Properties
+        public virtual Country Country { get; set; }
+        #endregion
+    }
+}
